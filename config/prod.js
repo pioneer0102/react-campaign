68acac2f117401f4050aafae8d8274573494f9ef
pioneer0102 <ehartdev0102@gmail.com>
2018-09-25 11:56:27 -0500

    Fixed typo in prod.js

--- config/prod.js
+++ config/prod.js
@@ -1,14 +1,4 @@
 // prod.js - production keys here
-
-module.exports = {
-  googleClientID:
-    "997294811117-bo15qtnt0hbf3jttk1q3m7b78lan0633.apps.googleusercontent.com",
-  googleClientSecret: "K-n6fIutkGqjVc0-CVOX3Uao",
-  mongoURI:
-    "mongodb://admin-mjgattas:2hTVrm6szH23Akw@ds113853.mlab.com:13853/react-campaign-app-prod",
-  cookieKey: "fasdlkfjmnmnvvxiefwjfdlsafdsafcccc"
-};
-
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
